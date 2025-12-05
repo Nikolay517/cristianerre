8856b799b7d53e5fe526c986261e6faf36753ee6
Nikolay517 <nicoquir192@gmail.com>
2025-12-04 23:21:56 -0300

    cristian

--- sitioweb/settings.py
+++ sitioweb/settings.py
@@ -66,9 +66,10 @@ WSGI_APPLICATION = 'sitioweb.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        default=os.environ.get('DATABASE_URL', {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        })
     )
 }
 
