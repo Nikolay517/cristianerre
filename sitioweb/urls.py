e325498ffffbdaf0c0a79c27f5899acae88a699b
Nikolay517 <nicoquir192@gmail.com>
2025-12-05 00:56:23 -0300

    vuelve

--- sitioweb/urls.py
+++ sitioweb/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+# IMPORTANTE: Esto es necesario para que funcione el cambio de contraseña
 from django.contrib.auth import views as auth_views 
 from core import views
+from django.urls import path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     # --- VISTAS PÚBLICAS ---
@@ -13,26 +16,33 @@ urlpatterns = [
     path('quienes/', views.quienes, name='quienes'),
     path('contacto/', views.contacto, name='contacto'),
 
-    # --- VISTAS DE GESTIÓN ---
+    # --- VISTAS DE GESTIÓN (Staff/Cristian) ---
     path('galeria/agregar/', views.agregar_obra, name='agregar_obra'),
     path('galeria/editar/<int:obra_id>/', views.editar_obra, name='editar_obra'),
     path('galeria/eliminar/<int:obra_id>/', views.eliminar_obra, name='eliminar_obra'),
 
-    # --- AUTENTICACIÓN ---
+    # --- AUTENTICACIÓN PERSONALIZADA (Tus diseños bonitos) ---
     path('registro/', views.registro, name='registro'),
     path('login/', views.iniciar_sesion, name='login'),
     path('logout/', views.cerrar_sesion, name='logout'),
 
-    # --- RECUPERACIÓN DE CONTRASEÑA ---
+    # --- RECUPERACIÓN DE CONTRASEÑA (Sistema Gmail) ---
+    # 1. Formulario para pedir el correo
     path('reset_password/', 
          auth_views.PasswordResetView.as_view(template_name="core/password/password_reset_form.html"), 
          name='password_reset'),
+
+    # 2. Mensaje de "Correo enviado exitosamente"
     path('reset_password_sent/', 
          auth_views.PasswordResetDoneView.as_view(template_name="core/password/password_reset_done.html"), 
          name='password_reset_done'),
+
+    # 3. Link especial que llega al correo para poner la nueva clave
     path('reset/<uidb64>/<token>/', 
          auth_views.PasswordResetConfirmView.as_view(template_name="core/password/password_reset_confirm.html"), 
          name='password_reset_confirm'),
+
+    # 4. Mensaje de "Contraseña cambiada correctamente"
     path('reset_password_complete/', 
          auth_views.PasswordResetCompleteView.as_view(template_name="core/password/password_reset_complete.html"), 
          name='password_reset_complete'),
@@ -41,5 +51,8 @@ urlpatterns = [
 ]
 
 # Configuración para ver imágenes en modo desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+re_media = r'^%s(?P<path>.*)$' % settings.MEDIA_URL.lstrip('/')
+urlpatterns += [re_path(re_media, serve, {'document_root': settings.MEDIA_ROOT})]
